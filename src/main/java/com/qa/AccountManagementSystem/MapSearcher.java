474df9a67ecc647d3fd843a5626ddcc0db41d090
AndrewHall2469 <AndrewHall978@gmail.com>
2018-04-19 15:24:30 +0100

    Added refactor

--- src/main/java/com/qa/AccountManagementSystem/MapSearcher.java
+++ src/main/java/com/qa/AccountManagementSystem/MapSearcher.java
@@ -4,16 +4,7 @@ import java.util.Map;
 
 public class MapSearcher {
 	public int findName (String firstName, Map<Integer, Account> accounts) {
-		int count = 0;
-				for (Account acn : accounts.values()) 
-				{
-					if (acn.getFirstName().equals(firstName)) 
-					{
-						count++;
-					}
-				}
-		System.out.println(count + " accounts are present with the same first names :-) " + firstName);
-		return count;
+		return (int)accounts.values().stream().filter(account -> firstName.equals(account.getFirstName())).count();
 		
 	}
 
